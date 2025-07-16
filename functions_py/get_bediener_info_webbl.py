77d1ecbbe48a6b2032e59f5bbf3d9447d3399b66
rudy-sindata <rudy.sindata@gmail.com>
2025-07-16 15:58:32 +0800

    Pindahkan isi manual edit

---
+++ functions_py/get_bediener_info_webbl.py
@@ -0,0 +1,167 @@
+#using conversion tools version: 1.0.0.117
+
+from functions.additional_functions import *
+from decimal import Decimal
+from models import Queasy, Bediener, Paramtext
+
+def get_bediener_info_webbl(user_name:string, user_init:string):
+
+    prepare_cache ([Queasy, Bediener, Paramtext])
+
+    epoch_signature = 0
+    bbediener = False
+    bediener_info_data = []
+    signature_list_data = []
+    dept_name:string = ""
+    queasy = bbediener = paramtext = None
+
+    bediener_info = value_list = signature_list = buff_user = bdept = totpdata = None
+
+    bediener_info_data, Bediener_info = create_model("Bediener_info", {"user_number":int, "user_init":string, "user_name":string, "dept_number":int, "dept_name":string, "email":string, "mobile":string, "pager":string, "totp_flag":bool, "totp_status":string})
+    value_list_data, Value_list = create_model("Value_list", {"var_name":string, "value_str":string})
+    signature_list_data, Signature_list = create_model("Signature_list", {"var_name":string, "signature":string})
+
+    Buff_user = create_buffer("Buff_user",Queasy)
+    Bdept = create_buffer("Bdept",Queasy)
+    Totpdata = create_buffer("Totpdata",Queasy)
+
+
+    db_session = local_storage.db_session
+
+    def generate_output():
+        nonlocal epoch_signature, bediener, bediener_info_data, signature_list_data, dept_name, queasy, bediener, paramtext
+        nonlocal user_name, user_init
+        nonlocal buff_user, bdept, totpdata
+
+
+        nonlocal bediener_info, value_list, signature_list, buff_user, bdept, totpdata
+        nonlocal bediener_info_data, value_list_data, signature_list_data
+
+        return {"epoch_signature": epoch_signature, "bediener": bediener, "bediener-info": bediener_info_data, "signature-list": signature_list_data}
+
+    def create_signature(user_name:string, value_list_data:[Value_list]):
+
+        nonlocal epoch_signature, bediener, bediener_info_data, signature_list_data, dept_name, queasy, bediener, paramtext
+        nonlocal user_init
+        nonlocal buff_user, bdept, totpdata
+
+
+        nonlocal bediener_info, value_list, signature_list, buff_user, bdept, totpdata
+        nonlocal bediener_info_data, signature_list_data
+
+        epoch = 0
+        dtz1 = None
+        dtz2 = None
+        lic_nr:string = ""
+        data:string = ""
+        value_str:string = ""
+
+        def generate_inner_output():
+            return (epoch, signature_list_data)
+
+
+        paramtext = get_cache (Paramtext, {"txtnr": [(eq, 243)]})
+
+        if paramtext and paramtext.ptexte != "":
+            lic_nr = decode_string(paramtext.ptexte)
+        dtz1 = get_current_datetime()
+        dtz2 = parse("1970-01-01T00:00:00.000+0:00")
+        epoch = get_interval(dtz1, dtz2, "milliseconds")
+
+        for value_list in query(value_list_data):
+            value_str = value_list.value_str.lower()
+
+            if value_str == "yes":
+                value_str = "true"
+            elif value_str == "no":
+                value_str = "false"
+            data = value_str + "-" + to_string(epoch) + "-" + to_string(lic_nr) + "-" + user_name.lower()
+            signature_list = Signature_list()
+            signature_list_data.append(signature_list)
+
+            signature_list.var_name = value_list.var_name
+            signature_list.signature = sha1(data).hexdigest()
+
+        return generate_inner_output()
+
+
+    def decode_string(in_str:string):
+
+        nonlocal epoch_signature, bediener, bediener_info_data, signature_list_data, dept_name, queasy, bediener, paramtext
+        nonlocal user_name, user_init
+        nonlocal buff_user, bdept, totpdata
+
+
+        nonlocal bediener_info, value_list, signature_list, buff_user, bdept, totpdata
+        nonlocal bediener_info_data, value_list_data, signature_list_data
+
+        out_str = ""
+        s:string = ""
+        j:int = 0
+        len_:int = 0
+
+        def generate_inner_output():
+            return (out_str)
+
+        s = in_str
+        j = asc(substring(s, 0, 1)) - 70
+        len_ = length(in_str) - 1
+        s = substring(in_str, 1, len_)
+        for len_ in range(1,length(s)  + 1) :
+            out_str = out_str + chr_unicode(asc(substring(s, len_ - 1, 1)) - j)
+
+        return generate_inner_output()
+
+
+    bbediener = get_cache (Bediener, {"userinit": [(eq, user_init)],"username": [(eq, user_name)]})
+
+    if bbediener:
+
+        buff_user = get_cache (Queasy, {"key": [(eq, 134)],"number1": [(eq, bbediener.nr)],"betriebsnr": [(eq, 0)],"deci1": [(eq, 0)],"logi1": [(eq, False)]})
+
+        bdept = get_cache (Queasy, {"key": [(eq, 19)],"number1": [(eq, bbediener.user_group)]})
+
+        if bdept:
+            dept_name = bdept.char3
+        else:
+            dept_name = ""
+        bediener_info = Bediener_info()
+        bediener_info_data.append(bediener_info)
+
+        bediener_info.user_number = bbediener.nr
+        bediener_info.user_init = bbediener.userinit
+        bediener_info.user_name = bbediener.username
+        bediener_info.dept_number = bbediener.user_group
+        bediener_info.dept_name = dept_name
+        bediener_info.email = buff_user.char2
+        bediener_info.mobile = buff_user.char1
+        bediener_info.pager = buff_user.char3
+
+        totpdata = get_cache (Queasy, {"key": [(eq, 341)],"char1": [(eq, bbediener.username)]})
+
+        if totpdata:
+            bediener_info.totp_flag = True
+
+            if totpdata.logi1 :
+                bediener_info.totp_status = "ACTIVE"
+            else:
+                bediener_info.totp_status = "INACTIVE"
+        else:
+            bediener_info.totp_flag = False
+        value_list = Value_list()
+        value_list_data.append(value_list)
+
+        value_list.var_name = "totp-flag"
+        value_list.value_str = to_string(bediener_info.totp_flag)
+
+
+        value_list = Value_list()
+        value_list_data.append(value_list)
+
+        value_list.var_name = "totp-status"
+        value_list.value_str = bediener_info.totp_status
+
+
+        epoch_signature, signature_list_data = create_signature(bbediener.username, value_list_data)
+
+    return generate_output()
