0072665192b194a99ee281cb7483a3f3d204ba43
rudy-sindata <rudy.sindata@gmail.com>
2025-07-31 09:16:02 +0800

    130, vhpFOR/rmFproductShowdetail, cr_fproduct_showdetail_webbl, optimize

--- functions_py/create_forecast_history_detail_cldbl.py
+++ functions_py/create_forecast_history_detail_cldbl.py
@@ -1,5 +1,10 @@
 #using conversion tools version: 1.0.0.117
 
+#-----------------------------------------
+# Rd, 31/7/2025
+# gitlab: 130
+# check datum None
+#-----------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from datetime import date
