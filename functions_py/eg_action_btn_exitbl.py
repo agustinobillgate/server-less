3cc2b3b6acf7dc292d3d5264b07366b0f4d5a6e8
rudy-sindata <rudy.sindata@gmail.com>
2025-08-01 10:22:49 +0800

    vhpENG/egActionBtnExit, if available

--- functions_py/eg_action_btn_exitbl.py
+++ functions_py/eg_action_btn_exitbl.py
@@ -1,5 +1,8 @@
 #using conversion tools version: 1.0.0.117
-
+#----------------------------------------
+# Rd, 1/8/2025
+# if available
+#----------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from models import Eg_action
@@ -42,27 +45,29 @@ def eg_action_btn_exitbl(action_data:[Action], case_type:int, rec_id:int, user_i
 
         eg_action = get_cache (Eg_action, {"_recid": [(eq, rec_id)]})
 
-        queri = get_cache (Eg_action, {"actionnr": [(eq, action.actionnr)],"_recid": [(ne, eg_action._recid)]})
-
-        if queri:
-            fl_code = 1
-
-            return generate_output()
-        else:
-
-            queri = get_cache (Eg_action, {"bezeich": [(eq, action.bezeich)],"maintask": [(eq, action.maintask)],"_recid": [(ne, eg_action._recid)]})
+        # Rd, 1/8/2025
+        if eg_action:
+            queri = get_cache (Eg_action, {"actionnr": [(eq, action.actionnr)],"_recid": [(ne, eg_action._recid)]})
 
             if queri:
-                fl_code = 2
+                fl_code = 1
 
                 return generate_output()
             else:
-                pass
-                buffer_copy(action, eg_action)
-                eg_action.usefor = a
+
+                queri = get_cache (Eg_action, {"bezeich": [(eq, action.bezeich)],"maintask": [(eq, action.maintask)],"_recid": [(ne, eg_action._recid)]})
+
+                if queri:
+                    fl_code = 2
+
+                    return generate_output()
+                else:
+                    pass
+                    buffer_copy(action, eg_action)
+                    eg_action.usefor = a
 
 
-                pass
-                fl_code = 3
+                    pass
+                    fl_code = 3
 
     return generate_output()
