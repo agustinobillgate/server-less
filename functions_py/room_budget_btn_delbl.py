f0df728307f429feeeecb9d22fb64004b44e45fb
rudy-sindata <rudy.sindata@gmail.com>
2025-08-04 21:31:39 +0800

    vhpSM/roomBudgetBtnDel

--- functions_py/room_budget_btn_delbl.py
+++ functions_py/room_budget_btn_delbl.py
@@ -1,5 +1,9 @@
 #using conversion tools version: 1.0.0.117
-
+#-----------------------------------------
+# Rd 04/08/2025
+# gitlab: -
+# remarks: -
+#-----------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from models import Rmbudget
@@ -17,8 +21,7 @@ def room_budget_btn_delbl(rec_id:int):
 
 
     rmbudget = get_cache (Rmbudget, {"_recid": [(eq, rec_id)]})
-    pass
-    db_session.delete(rmbudget)
-    pass
-
+    if rmbudget:
+        db_session.delete(rmbudget)
+    
     return generate_output()
