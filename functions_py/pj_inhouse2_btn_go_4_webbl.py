b2b330cd98e95e00e2433b61c49302ab25f7aa87
rudy-sindata <rudy.sindata@gmail.com>
2025-08-20 09:28:01 +0800

    vhpFOR/loadPJInhouse24, pj_inhouse2_btn_go_4_webbl, validasi pembagi nol

--- functions_py/pj_inhouse2_btn_go_4_webbl.py
+++ functions_py/pj_inhouse2_btn_go_4_webbl.py
@@ -260,7 +260,8 @@ def pj_inhouse2_btn_go_4_webbl(sorttype:int, datum:date, curr_date:date, curr_ga
 
         summary_list1.summ = ""
         summary_list1.room_type = "OCC. PAYING ROOMS (%)"
-        summary_list1.qty = to_string(tot_payrm / tot_avail * 100, "->>9.99")
+        # summary_list1.qty = to_string(tot_payrm / tot_avail * 100, "->>9.99")
+        summary_list1.qty = to_string(safe_divide(tot_payrm , (tot_avail * 100)), "->>9.99")
 
         for sum_list in query(sum_list_data):
             summary_list2 = Summary_list2()
