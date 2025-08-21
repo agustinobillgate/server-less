acd0cd435c98dfda3f81d59182d9a7be94892b4c
rudy-sindata <rudy.sindata@gmail.com>
2025-08-21 16:14:11 +0800

    FB Flash, transFER -> TRANSFER

--- functions_py/fb_flash_1bl.py
+++ functions_py/fb_flash_1bl.py
@@ -891,7 +891,7 @@ def fb_flash_1bl(pvilanguage:int, from_grp:int, food:int, bev:int, date1:date, d
         fbflash_list = Fbflash_list()
         fbflash_list_data.append(fbflash_list)
 
-        fbflash_list.trans_to_storage = translateExtended ("transFER TO SIDE STORE", lvcarea, "")
+        fbflash_list.trans_to_storage = translateExtended ("TRANSFER TO SIDE STORE", lvcarea, "")
         betrag =  to_decimal("0")
         t_betrag1 =  to_decimal("0")
 
@@ -1380,7 +1380,7 @@ def fb_flash_1bl(pvilanguage:int, from_grp:int, food:int, bev:int, date1:date, d
         fbflash_list = Fbflash_list()
         fbflash_list_data.append(fbflash_list)
 
-        fbflash_list.trans_to_storage = translateExtended ("transFER TO SIDE STORE", lvcarea, "")
+        fbflash_list.trans_to_storage = translateExtended ("TRANSFER TO SIDE STORE", lvcarea, "")
         betrag =  to_decimal("0")
         t_betrag1 =  to_decimal("0")
 
