4200b1067e7baae55afa2b6a2d16aad4e5006282
rudy-sindata <rudy.sindata@gmail.com>
2025-08-21 14:17:33 +0800

    vhpGL/getTrialBalance, trialbaiance_btn_executebl, data tidak tampil.

--- functions_py/trialbaiance_btn_executebl.py
+++ functions_py/trialbaiance_btn_executebl.py
@@ -1,5 +1,8 @@
 #using conversion tools version: 1.0.0.117
-
+#-----------------------------------------
+# Rd, 21/8/20225
+# data tidak tampil semua
+#-----------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from datetime import date
