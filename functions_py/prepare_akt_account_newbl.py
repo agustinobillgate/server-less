5c4f7d1ce2990a0ad81c04bc22b07a2f52706ad7
rudy-sindata <rudy.sindata@gmail.com>
2025-08-25 08:20:41 +0800

    vhpSM/aktAccountPrepare, prepare_akt_account_newbl,  name 'resstatus' is not defined

--- functions_py/prepare_akt_account_newbl.py
+++ functions_py/prepare_akt_account_newbl.py
@@ -1,4 +1,8 @@
 #using conversion tools version: 1.0.0.117
+#------------------------------------------
+# Rd, 25/8/2025
+# name 'resstatus' is not defined
+#------------------------------------------
 
 from functions.additional_functions import *
 from decimal import Decimal
