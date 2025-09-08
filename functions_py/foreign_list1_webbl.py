78274056c789725bf99c4085621f8ce11ad15772
rudy-sindata <rudy.sindata@gmail.com>
2025-09-08 09:13:08 +0800

    vhpFOR/foreignList1, beda data di range tanggal.

--- functions_py/foreign_list1_webbl.py
+++ functions_py/foreign_list1_webbl.py
@@ -1,4 +1,8 @@
 #using conversion tools version: 1.0.0.117
+#------------------------------------------
+# Rd, 8/9/2025
+# beda data di range tanggal
+#------------------------------------------
 
 from functions.additional_functions import *
 from decimal import Decimal
