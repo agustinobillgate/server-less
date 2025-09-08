fba5e9ea793eae863c5539a79b7ba6c73b29ce2e
rudy-sindata <rudy.sindata@gmail.com>
2025-09-08 15:33:43 +0800

    Update fo-invoice-fill-rescommentBL.p, bemerk -> bemerkung

--- functions_py/log_program_rd.py
+++ functions_py/log_program_rd.py
@@ -5,11 +5,11 @@ LOG_DIR = "/usr1/serverless/src/logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 log_file_path = os.path.join(LOG_DIR, "rd_log.txt")
 
-def write_log(level, message):
+def rwrite_log(level, message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_message = f"[{timestamp}] [{level.upper()}] {message}\n"
 
-    with open(log_file_path, "a") as log_file:
+    with open(log_file_path, "w") as log_file:
         log_file.write(formatted_message)
 
     print(formatted_message.strip())
