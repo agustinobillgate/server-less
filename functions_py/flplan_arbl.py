b91a8067fd4c0338e8d1ec48449f41f4ce1f2bac
rudy-sindata <rudy.sindata@gmail.com>
2025-09-10 15:54:33 +0800

    Replace g-info, r_info, \\n -> \n

--- functions_py/flplan_arbl.py
+++ functions_py/flplan_arbl.py
@@ -1,5 +1,8 @@
 #using conversion tools version: 1.0.0.117
-
+#------------------------------------------
+# Rd, 10/9/2025
+# replace g-info, c-info : \\n -> \n
+#------------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from datetime import date
