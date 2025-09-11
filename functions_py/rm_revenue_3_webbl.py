5e0dab1f853b711ae1d90b22bfa305d275ff33a5
rudy-sindata <rudy.sindata@gmail.com>
2025-09-11 09:57:23 +0800

    prepare_rm_atdrecapbl, beda baris TOTAL

--- functions_py/rm_revenue_3_webbl.py
+++ functions_py/rm_revenue_3_webbl.py
@@ -645,12 +645,17 @@ def rm_revenue_3_webbl(m_ftd:bool, m_ytd:bool, f_date:date, t_date:date, to_date
                             if excl_compl:
 
                                 genstat = db_session.query(Genstat).filter(
-                                         (Genstat.datum == datum) & (Genstat.zikatnr == zimmer.zikatnr) & (Genstat.zinr == zimmer.zinr) & (Genstat.zipreis == 0) & (Genstat.gratis != 0) & (Genstat.resstatus == 6) & (Genstat.res_logic[inc_value(1)])).first()
-
+                                         (Genstat.datum == datum) & 
+                                         (Genstat.zikatnr == zimmer.zikatnr) & 
+                                         (Genstat.zinr == zimmer.zinr) & 
+                                         (Genstat.zipreis == 0) & 
+                                         (Genstat.gratis != 0) & 
+                                         (Genstat.resstatus == 6) & 
+                                         (Genstat.res_logic[inc_value(1)])).first()
+                                # genstat -> issue, tanya pak Fadly
                                 if genstat:
                                     do_it = False
                                 else:
-
                                     segment = db_session.query(Segment).filter(
                                              (Segment.segmentcode == genstat.segmentcode) & ((Segment.betriebsnr == 1) | (Segment.betriebsnr == 2))).first()
 
