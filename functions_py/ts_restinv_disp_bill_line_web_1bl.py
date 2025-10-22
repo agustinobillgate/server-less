645e6e4615d0be15787fb979ea3d635944e87ec5
Oscar Deladas <oscar_deladas@sindata.net>
2025-10-22 16:45:49 +0800

    Oscar - 1DA62E

--- functions_py/ts_restinv_disp_bill_line_web_1bl.py
+++ functions_py/ts_restinv_disp_bill_line_web_1bl.py
@@ -12,13 +12,13 @@ from functions.additional_functions import *
 from decimal import Decimal
 from datetime import date
 from functions.htplogic import htplogic
-from functions.ts_restinv_disp_requestbl import ts_restinv_disp_requestbl
 from sqlalchemy import func
-from models import H_bill_line, H_artikel, Htparam, Hoteldpt, H_journal, Wgrpgen, Artikel
+from functions.ts_restinv_disp_requestbl import ts_restinv_disp_requestbl
+from models import H_bill_line, H_artikel, Htparam, Hoteldpt, H_journal, Wgrpgen, Artikel, H_mjourn
 
 def ts_restinv_disp_bill_line_web_1bl(double_currency:bool, rechnr:int, curr_dept:int, avail_t_h_bill:bool, hbill_recid:int):
 
-    prepare_cache ([H_artikel, Htparam, Hoteldpt, H_journal, Wgrpgen, Artikel])
+    prepare_cache ([H_artikel, Htparam, Hoteldpt, H_journal, Wgrpgen, Artikel, H_mjourn])
 
     t_h_bill_line_data = []
     summary_bill_data = []
@@ -65,37 +65,50 @@ def ts_restinv_disp_bill_line_web_1bl(double_currency:bool, rechnr:int, curr_dep
     grand_tot:Decimal = to_decimal("0.0")
     cashless_flag:bool = False
     cashless_artnr:int = None
-    h_bill_line = h_artikel = htparam = hoteldpt = h_journal = wgrpgen = artikel = None
+    h_bill_line_recid_main:int = 0
+    request_str:string = ""
+    h_bill_line = h_artikel = htparam = hoteldpt = h_journal = wgrpgen = artikel = h_mjourn = None
 
-    t_h_bill_line = ordered_item = summary_bill = t_h_artikel = None
+    t_h_bill_line = ordered_item = summary_bill = t_h_artikel = h_artikel_buff = None
 
-    t_h_bill_line_data, T_h_bill_line = create_model_like(H_bill_line, {"rec_id":int, "request_str":string, "flag_code":int, "kds_stat":int, "kds_stat_str":string, "posting_id":int, "t_time":string, "artart":int}, {"kds_stat": -1})
+    t_h_bill_line_data, T_h_bill_line = create_model_like(H_bill_line, {"rec_id":int, "request_str":string, "flag_code":int, "kds_stat":int, "kds_stat_str":string, "posting_id":int, "t_time":string, "artart":int, "menu_flag":int, "sub_menu_bezeich":string, "sub_menu_betriebsnr":int, "sub_menu_qty":int}, {"kds_stat": -1})
     ordered_item_data, Ordered_item = create_model("Ordered_item", {"dept":int, "artnr":int, "rec_id":int, "qty":int, "epreis":Decimal, "net_bet":Decimal, "tax":Decimal, "service":Decimal, "bill_date":date, "betrag":Decimal})
     summary_bill_data, Summary_bill = create_model("Summary_bill", {"subtotal":Decimal, "total_service":Decimal, "total_tax":Decimal, "grand_total":Decimal, "gt_not_round":Decimal})
     t_h_artikel_data, T_h_artikel = create_model_like(H_artikel, {"rec_id":int})
 
+    H_artikel_buff = create_buffer("H_artikel_buff",H_artikel)
+
+
     db_session = local_storage.db_session
 
     def generate_output():
-        nonlocal t_h_bill_line_data, summary_bill_data, t_h_artikel_data, amount_bill, depo_flag, rsvtable_text, t_serv_perc, t_mwst_perc, t_fact, t_service, t_mwst1, t_mwst, h_service, h_mwst, h_mwst2, t_h_service, t_h_mwst, t_h_mwst2, incl_service, incl_mwst, gst_logic, serv_disc, vat_disc, f_discart, amount, f_dec, serv_code, vat_code, servtax_use_foart, serv_vat, tax_vat, ct, l_deci, fact_scvat, service, vat, vat2, mwst, mwst1, sub_tot, tot_serv, tot_tax, grand_tot, cashless_flag, cashless_artnr, h_bill_line, h_artikel, htparam, hoteldpt, h_journal, wgrpgen, artikel
+        nonlocal t_h_bill_line_data, summary_bill_data, t_h_artikel_data, amount_bill, depo_flag, rsvtable_text, t_serv_perc, t_mwst_perc, t_fact, t_service, t_mwst1, t_mwst, h_service, h_mwst, h_mwst2, t_h_service, t_h_mwst, t_h_mwst2, incl_service, incl_mwst, gst_logic, serv_disc, vat_disc, f_discart, amount, f_dec, serv_code, vat_code, servtax_use_foart, serv_vat, tax_vat, ct, l_deci, fact_scvat, service, vat, vat2, mwst, mwst1, sub_tot, tot_serv, tot_tax, grand_tot, cashless_flag, cashless_artnr, h_bill_line_recid_main, request_str, h_bill_line, h_artikel, htparam, hoteldpt, h_journal, wgrpgen, artikel, h_mjourn
         nonlocal double_currency, rechnr, curr_dept, avail_t_h_bill, hbill_recid
+        nonlocal h_artikel_buff
 
 
-        nonlocal t_h_bill_line, ordered_item, summary_bill, t_h_artikel
+        nonlocal t_h_bill_line, ordered_item, summary_bill, t_h_artikel, h_artikel_buff
         nonlocal t_h_bill_line_data, ordered_item_data, summary_bill_data, t_h_artikel_data
 
         return {"t-h-bill-line": t_h_bill_line_data, "summary-bill": summary_bill_data, "t-h-artikel": t_h_artikel_data, "amount_bill": amount_bill, "depo_flag": depo_flag, "rsvtable_text": rsvtable_text}
 
     def show_submenu():
 
-        nonlocal t_h_bill_line_data, summary_bill_data, t_h_artikel_data, amount_bill, depo_flag, rsvtable_text, t_serv_perc, t_mwst_perc, t_fact, t_service, t_mwst1, t_mwst, h_service, h_mwst, h_mwst2, t_h_service, t_h_mwst, t_h_mwst2, incl_service, incl_mwst, gst_logic, serv_disc, vat_disc, f_discart, amount, f_dec, serv_code, vat_code, servtax_use_foart, serv_vat, tax_vat, ct, l_deci, fact_scvat, service, vat, vat2, mwst, mwst1, sub_tot, tot_serv, tot_tax, grand_tot, cashless_flag, cashless_artnr, h_bill_line, h_artikel, htparam, hoteldpt, h_journal, wgrpgen, artikel
+        nonlocal t_h_bill_line_data, summary_bill_data, t_h_artikel_data, amount_bill, depo_flag, rsvtable_text, t_serv_perc, t_mwst_perc, t_fact, t_service, t_mwst1, t_mwst, h_service, h_mwst, h_mwst2, t_h_service, t_h_mwst, t_h_mwst2, incl_service, incl_mwst, gst_logic, serv_disc, vat_disc, f_discart, amount, f_dec, serv_code, vat_code, servtax_use_foart, serv_vat, tax_vat, ct, l_deci, fact_scvat, service, vat, vat2, mwst, mwst1, sub_tot, tot_serv, tot_tax, grand_tot, cashless_flag, cashless_artnr, h_bill_line_recid_main, request_str, h_bill_line, h_artikel, htparam, hoteldpt, h_journal, wgrpgen, artikel, h_mjourn
         nonlocal double_currency, rechnr, curr_dept, avail_t_h_bill, hbill_recid
+        nonlocal h_artikel_buff
 
 
-        nonlocal t_h_bill_line, ordered_item, summary_bill, t_h_artikel
+        nonlocal t_h_bill_line, ordered_item, summary_bill, t_h_artikel, h_artikel_buff
         nonlocal t_h_bill_line_data, ordered_item_data, summary_bill_data, t_h_artikel_data
 
         h_art2 = None
+        rqst_sub_menu:string = ""
+        list_reqst:List[string] = create_empty_list(15,"")
+        list_rec_id:List[int] = create_empty_list(15,0)
+        counter:int = 0
+        nr:int = 0
+        h_bill_line_recid:int = 0
         H_art2 =  create_buffer("H_art2",H_artikel)
 
         h_art2 = get_cache (H_artikel, {"departement": [(eq, h_bill_line.departement)],"artnr": [(eq, h_bill_line.artnr)]})
@@ -105,6 +118,60 @@ def ts_restinv_disp_bill_line_web_1bl(double_currency:bool, rechnr:int, curr_dep
 
         elif h_art2.artart == 0 and h_art2.betriebsnr > 0:
             t_h_bill_line.flag_code = 1
+            t_h_bill_line.sub_menu_betriebsnr = h_art2.betriebsnr
+
+            h_mjourn_obj_list = {}
+            h_mjourn = H_mjourn()
+            h_artikel_buff = H_artikel()
+            for h_mjourn.nr, h_mjourn.anzahl, h_mjourn.request, h_mjourn._recid, h_artikel_buff.betriebsnr, h_artikel_buff.departement, h_artikel_buff.artart, h_artikel_buff._recid, h_artikel_buff.bezeich, h_artikel_buff.artnr in db_session.query(H_mjourn.nr, H_mjourn.anzahl, H_mjourn.request, H_mjourn._recid, H_artikel_buff.betriebsnr, H_artikel_buff.departement, H_artikel_buff.artart, H_artikel_buff._recid, H_artikel_buff.bezeich, H_artikel_buff.artnr).join(H_artikel_buff,(H_artikel_buff.artnr == H_mjourn.artnr) & (H_artikel_buff.departement == H_mjourn.departement)).filter(
+                     (H_mjourn.nr == h_art2.betriebsnr) & (H_mjourn.departement == h_art2.departement) & (H_mjourn.rechnr == h_bill_line.rechnr)).order_by(H_mjourn._recid).all():
+                if h_mjourn_obj_list.get(h_mjourn._recid):
+                    continue
+                else:
+                    h_mjourn_obj_list[h_mjourn._recid] = True
+
+
+                counter = counter + 1
+
+                if num_entries(h_mjourn.request, "|") > 1:
+
+                    if entry(0, h_mjourn.request, "|") == to_string(h_bill_line._recid):
+                        t_h_bill_line = T_h_bill_line()
+                        t_h_bill_line_data.append(t_h_bill_line)
+
+                        t_h_bill_line.menu_flag = 2
+                        t_h_bill_line.sub_menu_bezeich = h_artikel_buff.bezeich
+                        t_h_bill_line.sub_menu_betriebsnr = h_mjourn.nr
+                        t_h_bill_line.sub_menu_qty = h_mjourn.anzahl
+                        t_h_bill_line.rec_id = h_bill_line._recid
+                        t_h_bill_line.artnr = h_artikel_buff.artnr
+                        t_h_bill_line.request_str = entry(1, h_mjourn.request, "|")
+                        t_h_bill_line.rechnr = None
+                        t_h_bill_line.bill_datum = None
+                        t_h_bill_line.anzahl = None
+                        t_h_bill_line.epreis =  to_decimal(None)
+                        t_h_bill_line.betrag =  to_decimal(None)
+                        t_h_bill_line.steuercode = None
+                        t_h_bill_line.bezeich = None
+                        t_h_bill_line.fremdwbetrag =  to_decimal(None)
+                        t_h_bill_line.zeit = None
+                        t_h_bill_line.waehrungsnr = None
+                        t_h_bill_line.sysdate = None
+                        t_h_bill_line.departement = None
+                        t_h_bill_line.prtflag = None
+                        t_h_bill_line.tischnr = None
+                        t_h_bill_line.kellner_nr = None
+                        t_h_bill_line.nettobetrag =  to_decimal(None)
+                        t_h_bill_line.paid_flag = None
+                        t_h_bill_line.betriebsnr = None
+                        t_h_bill_line.segmentcode = None
+                        t_h_bill_line.transferred = None
+                        t_h_bill_line.flag_code = None
+                        t_h_bill_line.kds_stat = None
+                        t_h_bill_line.kds_stat_st = None
+                        t_h_bill_line.posting_id = None
+                        t_h_bill_line.t_time = None
+                        t_h_bill_line.artart = None
 
     htparam = get_cache (Htparam, {"paramnr": [(eq, 468)]})
 
@@ -178,16 +245,19 @@ def ts_restinv_disp_bill_line_web_1bl(double_currency:bool, rechnr:int, curr_dep
             if h_journal:
                 t_h_bill_line.posting_id = h_journal.kellner_nr
 
-            # Rulita Added find first h_artikel TiketID:1DA62E
 
-            h_artikel = get_cache (H_artikel, {"artnr": [(eq, h_bill_line.artnr)], "departement": [(eq, h_bill_line.departement)]})
+            else:
+
+                h_journal = db_session.query(H_journal).filter(
+                         (H_journal.rechnr == h_bill_line.rechnr) & (H_journal.zeit == h_bill_line.zeit) & (matches(H_journal.bezeich,"*Discount*")) | (matches(H_journal.bezeich,"*Disc*"))).first()
+
+                if h_journal:
+                    t_h_bill_line.posting_id = h_journal.kellner_nr
+
+            h_artikel = get_cache (H_artikel, {"artnr": [(eq, h_bill_line.artnr)],"departement": [(eq, h_bill_line.departement)]})
 
             if h_artikel:
-                t_h_bill_line.artnr = h_artikel.artnr
-
-            # Rulita End added h_artikel TiketID:1DA62E
-
-            show_submenu()
+                t_h_bill_line.artart = h_artikel.artart
             request_str = get_output(ts_restinv_disp_requestbl(h_bill_line._recid))
             t_h_bill_line.request_str = request_str
             amount_bill =  to_decimal(amount_bill) + to_decimal(h_bill_line.betrag)
@@ -205,32 +275,40 @@ def ts_restinv_disp_bill_line_web_1bl(double_currency:bool, rechnr:int, curr_dep
             ordered_item.bill_date = t_h_bill_line.bill_datum
             ordered_item.betrag =  to_decimal(t_h_bill_line.betrag)
 
+
+            show_submenu()
+
     else:
 
         for h_bill_line in db_session.query(H_bill_line).filter(
                  (H_bill_line.rechnr == rechnr) & (H_bill_line.departement == curr_dept)).order_by(H_bill_line.bill_datum.desc(), H_bill_line.zeit.desc()).yield_per(100):
+            h_bill_line_recid_main = h_bill_line._recid
             t_h_bill_line = T_h_bill_line()
             t_h_bill_line_data.append(t_h_bill_line)
 
             buffer_copy(h_bill_line, t_h_bill_line)
             t_h_bill_line.rec_id = h_bill_line._recid
             t_h_bill_line.t_time = to_string(h_bill_line.zeit, "HH:MM:SS")
+            t_h_bill_line.menu_flag = 1
 
             h_journal = get_cache (H_journal, {"rechnr": [(eq, h_bill_line.rechnr)],"zeit": [(eq, h_bill_line.zeit)],"schankbuch": [(eq, to_int(h_bill_line._recid))]})
 
             if h_journal:
                 t_h_bill_line.posting_id = h_journal.kellner_nr
 
-            # Rulita Added find first h_artikel TiketID:1DA62E
 
-            h_artikel = get_cache (H_artikel, {"artnr": [(eq, h_bill_line.artnr)], "departement": [(eq, h_bill_line.departement)]})
+            else:
+
+                h_journal = db_session.query(H_journal).filter(
+                         (H_journal.rechnr == h_bill_line.rechnr) & (H_journal.zeit == h_bill_line.zeit) & (matches(H_journal.bezeich,"*Discount*")) | (matches(H_journal.bezeich,"*Disc*"))).first()
+
+                if h_journal:
+                    t_h_bill_line.posting_id = h_journal.kellner_nr
+
+            h_artikel = get_cache (H_artikel, {"artnr": [(eq, h_bill_line.artnr)],"departement": [(eq, h_bill_line.departement)]})
 
             if h_artikel:
-                t_h_bill_line.artnr = h_artikel.artnr
-
-            # Rulita End added h_artikel TiketID:1DA62E
-
-            show_submenu()
+                t_h_bill_line.artart = h_artikel.artart
             request_str = get_output(ts_restinv_disp_requestbl(h_bill_line._recid))
             t_h_bill_line.request_str = request_str
             amount_bill =  to_decimal(amount_bill) + to_decimal(h_bill_line.betrag)
@@ -247,6 +325,9 @@ def ts_restinv_disp_bill_line_web_1bl(double_currency:bool, rechnr:int, curr_dep
             ordered_item.net_bet =  to_decimal(t_h_bill_line.nettobetrag)
             ordered_item.bill_date = t_h_bill_line.bill_datum
             ordered_item.betrag =  to_decimal(t_h_bill_line.betrag)
+
+
+            show_submenu()
 
 
     wgrpgen = db_session.query(Wgrpgen).filter(
