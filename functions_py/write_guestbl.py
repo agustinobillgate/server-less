fb98e61b8e7bb105251d058c5364d1002ef2be08
rudy-sindata <rudy.sindata@gmail.com>
2025-10-27 18:00:37 +0800

    update write data

--- functions_py/write_guestbl.py
+++ functions_py/write_guestbl.py
@@ -25,18 +25,18 @@ def write_guestbl(case_type:int, t_guest_data:[T_guest]):
 
         return {"success_flag": success_flag}
 
-    t_guest = query(t_guest_data, first=True)
+    # t_guest = query(t_guest_data, first=True)
+    t_guest = t_guest_data[0] if t_guest_data else None
 
     if case_type == 1:
 
-        guest = get_cache (Guest, {"gastnr": [(eq, t_guest.gastnr)]})
+        # guest = get_cache (Guest, {"gastnr": [(eq, t_guest.gastnr)]})
+        guest = db_session.query(Guest).filter(Guest.gastnr == t_guest.gastnr).first()
 
         if guest:
             buffer_copy(t_guest, guest)
             guest.karteityp = t_guest.karteityp
             db_session.commit()
-            pass
-            pass
             success_flag = True
     elif case_type == 2:
         guest = Guest()
