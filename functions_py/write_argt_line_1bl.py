73bcb778890704f8d7bc88627c796607223bdfe3
rudy-sindata <rudy.sindata@gmail.com>
2025-11-27 14:13:36 +0800

    Update locking

--- functions_py/write_argt_line_1bl.py
+++ functions_py/write_argt_line_1bl.py
@@ -1,5 +1,7 @@
 #using conversion tools version: 1.0.0.117
-
+#-------------------------------------------------------
+# Rd, 27/11/2025, with_for_update added
+#-------------------------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from models import Argt_line, Bediener, Res_history
@@ -41,8 +43,21 @@ def write_argt_line_1bl(user_init:string, argtlinebuff_data:[Argtlinebuff], t_ar
 
     if argtlinebuff and t_argt_line:
 
-        argt_line = get_cache (Argt_line, {"argtnr": [(eq, argtlinebuff.argtnr)],"argt_artnr": [(eq, argtlinebuff.argt_artnr)],"departement": [(eq, argtlinebuff.departement)],"fakt_modus": [(eq, argtlinebuff.fakt_modus)],"intervall": [(eq, argtlinebuff.intervall)],"kind1": [(eq, argtlinebuff.kind1)],"kind2": [(eq, argtlinebuff.kind2)],"betrag": [(eq, argtlinebuff.betrag)],"betriebsnr": [(eq, argtlinebuff.betriebsnr)],"vt_percnt": [(eq, argtlinebuff.vt_percnt)]})
-
+        # argt_line = get_cache (Argt_line, {"argtnr": [(eq, argtlinebuff.argtnr)],"argt_artnr": [(eq, argtlinebuff.argt_artnr)],
+        # "departement": [(eq, argtlinebuff.departement)],"fakt_modus": [(eq, argtlinebuff.fakt_modus)],
+        # "intervall": [(eq, argtlinebuff.intervall)],"kind1": [(eq, argtlinebuff.kind1)],"kind2": [(eq, argtlinebuff.kind2)],
+        # "betrag": [(eq, argtlinebuff.betrag)],"betriebsnr": [(eq, argtlinebuff.betriebsnr)],"vt_percnt": [(eq, argtlinebuff.vt_percnt)]})
+        argt_line = db_session.query(Argt_line).filter(
+                 (Argt_line.argtnr == argtlinebuff.argtnr) &
+                    (Argt_line.argt_artnr == argtlinebuff.argt_artnr) &
+                    (Argt_line.departement == argtlinebuff.departement) &
+                    (Argt_line.fakt_modus == argtlinebuff.fakt_modus) &
+                    (Argt_line.intervall == argtlinebuff.intervall) &
+                    (Argt_line.kind1 == argtlinebuff.kind1) &
+                    (Argt_line.kind2 == argtlinebuff.kind2) &
+                    (Argt_line.betrag == argtlinebuff.betrag) &
+                    (Argt_line.betriebsnr == argtlinebuff.betriebsnr) &
+                    (Argt_line.vt_percnt == argtlinebuff.vt_percnt)).with_for_update().first()
         if argt_line:
             betrag =  to_decimal(t_argt_line.betrag)
 
