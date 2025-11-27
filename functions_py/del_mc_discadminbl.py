30b60cd2b830d611433160d3f5ae9e8cb6da074d
rudy-sindata <rudy.sindata@gmail.com>
2025-11-27 17:38:05 +0800

    Update locking

--- functions_py/del_mc_discadminbl.py
+++ functions_py/del_mc_discadminbl.py
@@ -1,5 +1,7 @@
 #using conversion tools version: 1.0.0.117
-
+#-------------------------------------------------------
+# Rd, 27/11/2025, with_for_update added
+#-------------------------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from models import Hoteldpt, Mc_disc
@@ -25,7 +27,10 @@ def del_mc_discadminbl(curr_nr:int, rec_id:int):
         return {}
 
 
-    mc_disc = get_cache (Mc_disc, {"nr": [(eq, curr_nr)],"_recid": [(eq, rec_id)]})
+    # mc_disc = get_cache (Mc_disc, {"nr": [(eq, curr_nr)],"_recid": [(eq, rec_id)]})
+    mc_disc = db_session.query(Mc_disc).filter(
+             (Mc_disc.nr == curr_nr) &
+             (Mc_disc._recid == rec_id)).with_for_update().first()
 
     if mc_disc:
         pass
