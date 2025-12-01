8c716fddcf99678670b7c62b2f3e2709ce27dd78
rudy-sindata <rudy.sindata@gmail.com>
2025-12-01 14:16:24 +0800

    PC update locking

--- functions_py/mk_pr_up_lorderhdrbl.py
+++ functions_py/mk_pr_up_lorderhdrbl.py
@@ -1,9 +1,12 @@
 #using conversion tools version: 1.0.0.117
-
+#-------------------------------------------------------
+# Rd, 01/12/2025, with_for_update added
+#-------------------------------------------------------
 from functions.additional_functions import *
 from decimal import Decimal
 from datetime import date
 from models import L_orderhdr
+from sqlalchemy.orm import flag_modified
 
 def mk_pr_up_lorderhdrbl(case_type:int, rec_id:int, dept:int, datum:date):
 
@@ -20,7 +23,9 @@ def mk_pr_up_lorderhdrbl(case_type:int, rec_id:int, dept:int, datum:date):
         return {}
 
 
-    l_orderhdr = get_cache (L_orderhdr, {"_recid": [(eq, rec_id)]})
+    # l_orderhdr = get_cache (L_orderhdr, {"_recid": [(eq, rec_id)]})
+    l_orderhdr = db_session.query(L_orderhdr).filter(
+                 (L_orderhdr._recid == rec_id)).with_for_update().first()
 
     if case_type == 1:
         pass
@@ -30,5 +35,6 @@ def mk_pr_up_lorderhdrbl(case_type:int, rec_id:int, dept:int, datum:date):
         pass
         l_orderhdr.lieferdatum = datum
     pass
+    flag_modified(l_orderhdr, "angebot_lief")
 
     return generate_output()
