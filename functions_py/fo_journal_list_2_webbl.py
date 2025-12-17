f4b8d11e0657771146546feb3520eaa438af72bf
Oscar Deladas <oscar_deladas@sindata.net>
2025-12-17 15:48:42 +0700

    Oscar - Fix FO Transaction Report

--- functions_py/fo_journal_list_2_webbl.py
+++ functions_py/fo_journal_list_2_webbl.py
@@ -6,6 +6,10 @@ from datetime import date
 from functions.fo_journal_cld_3bl import fo_journal_cld_3bl
 from models import Queasy, Artikel, Htparam
 
+from functions import log_program as lp
+
+import traceback
+
 def fo_journal_list_2_webbl(from_art:int, to_art:int, from_dept:int, to_dept:int, from_date:date, to_date:date, sorttype:int, exclude_artrans:bool, long_digit:bool, foreign_flag:bool, onlyjournal:bool, excljournal:bool, mi_post:bool, mi_showrelease:bool, mi_break:bool, id_flag:string):
 
     prepare_cache ([Queasy, Artikel, Htparam])
@@ -35,81 +39,87 @@ def fo_journal_list_2_webbl(from_art:int, to_art:int, from_dept:int, to_dept:int
         nonlocal output_list_data, fo_journal_list_data
 
         return {"fo-journal-list": fo_journal_list_data, "done_flag": done_flag}
+    
 
-    queasy = Queasy()
-    db_session.add(queasy)
+    try:
+        queasy = Queasy()
+        db_session.add(queasy)
 
-    queasy.key = 285
-    queasy.char1 = "FO Transaction"
-    queasy.number1 = 1
-    queasy.char2 = id_flag
+        queasy.key = 285
+        queasy.char1 = "FO Transaction"
+        queasy.number1 = 1
+        queasy.char2 = id_flag
 
 
-    pass
-    gtot, output_list_data = get_output(fo_journal_cld_3bl(from_art, to_art, from_dept, to_dept, from_date, to_date, sorttype, exclude_artrans, long_digit, foreign_flag, onlyjournal, excljournal, mi_post, mi_showrelease, mi_break, id_flag))
+        pass
+        gtot, output_list_data = get_output(fo_journal_cld_3bl(from_art, to_art, from_dept, to_dept, from_date, to_date, sorttype, exclude_artrans, long_digit, foreign_flag, onlyjournal, excljournal, mi_post, mi_showrelease, mi_break, id_flag))
 
-    bqueasy = db_session.query(Queasy).filter((Queasy.key == 285) & (Queasy.char1 == "FO Transaction") & (Queasy.char2 == id_flag)).first() 
-    if bqueasy:
-        done_flag = True
-        db_session.refresh(bqueasy, with_for_update=True)
-        bqueasy.number1 = 0
+        bqueasy = db_session.query(Queasy).filter((Queasy.key == 285) & (Queasy.char1 == "FO Transaction") & (Queasy.char2 == id_flag)).first() 
+        if bqueasy:
+            done_flag = True
+            db_session.refresh(bqueasy, with_for_update=True)
+            bqueasy.number1 = 0
 
-    fo_journal_list_data.clear()
+        fo_journal_list_data.clear()
 
-    for output_list in query(output_list_data):
-        fo_journal_list = Fo_journal_list()
-        fo_journal_list_data.append(fo_journal_list)
+        for output_list in query(output_list_data):
+            fo_journal_list = Fo_journal_list()
+            fo_journal_list_data.append(fo_journal_list)
 
-        fo_journal_list.datum = date_mdy(substring(output_list.str, 0, 8))
-        fo_journal_list.c = output_list.c
-        fo_journal_list.roomnumber = output_list.zinr
-        fo_journal_list.nsflag = output_list.ns
-        fo_journal_list.mbflag = output_list.mb
-        fo_journal_list.shift = output_list.shift
-        fo_journal_list.billno = to_int(substring(output_list.str, 14, 9))
-        fo_journal_list.artno = to_int(substring(output_list.str, 23, 9))
-        fo_journal_list.bezeich = output_list.descr
-        fo_journal_list.voucher = output_list.voucher
-        fo_journal_list.depart = substring(output_list.str, 82, 12)
-        fo_journal_list.outlet = substring(output_list.str, 94, 6)
-        fo_journal_list.qty = to_int(substring(output_list.str, 100, 5))
-        fo_journal_list.amount = to_decimal(substring(output_list.str, 105, 22))
-        fo_journal_list.guestname = output_list.guestname
-        fo_journal_list.billrcvr = output_list.gname
-        fo_journal_list.zeit = substring(output_list.str, 127, 8)
-        fo_journal_list.id = substring(output_list.str, 135, 4)
-        fo_journal_list.sysdate = date_mdy(substring(output_list.str, 139, 8))
-        fo_journal_list.remark = output_list.remark
-        fo_journal_list.checkin = output_list.checkin
-        fo_journal_list.checkout = output_list.checkout
-        fo_journal_list.segcode = output_list.segcode
-        fo_journal_list.deptno = output_list.deptno
-        fo_journal_list.nationality = output_list.nationality
-        fo_journal_list.resnr = output_list.resnr
-        fo_journal_list.book_source = output_list.book_source
-        fo_journal_list.resname = output_list.resname
+            fo_journal_list.datum = substring(output_list.str, 0, 8)
+            fo_journal_list.c = output_list.c
+            fo_journal_list.roomnumber = output_list.zinr
+            fo_journal_list.nsflag = output_list.ns
+            fo_journal_list.mbflag = output_list.mb
+            fo_journal_list.shift = output_list.shift
+            fo_journal_list.billno = to_int(substring(output_list.str, 14, 9))
+            fo_journal_list.artno = to_int(substring(output_list.str, 23, 9))
+            fo_journal_list.bezeich = output_list.descr
+            fo_journal_list.voucher = output_list.voucher
+            fo_journal_list.depart = substring(output_list.str, 82, 12)
+            fo_journal_list.outlet = substring(output_list.str, 94, 6)
+            fo_journal_list.qty = to_int(substring(output_list.str, 100, 5))
+            fo_journal_list.amount = to_decimal(substring(output_list.str, 105, 22))
+            fo_journal_list.guestname = output_list.guestname
+            fo_journal_list.billrcvr = output_list.gname
+            fo_journal_list.zeit = substring(output_list.str, 127, 8)
+            fo_journal_list.id = substring(output_list.str, 135, 4)
+            fo_journal_list.sysdate = substring(output_list.str, 139, 8)
+            fo_journal_list.remark = output_list.remark
+            fo_journal_list.checkin = output_list.checkin
+            fo_journal_list.checkout = output_list.checkout
+            fo_journal_list.segcode = output_list.segcode
+            fo_journal_list.deptno = output_list.deptno
+            fo_journal_list.nationality = output_list.nationality
+            fo_journal_list.resnr = output_list.resnr
+            fo_journal_list.book_source = output_list.book_source
+            fo_journal_list.resname = output_list.resname
 
-        if mi_break:
-            fo_journal_list.amt_nett =  to_decimal(output_list.amt_nett)
-            fo_journal_list.service =  to_decimal(output_list.service)
-            fo_journal_list.vat =  to_decimal(output_list.vat)
+            if mi_break:
+                fo_journal_list.amt_nett =  to_decimal(output_list.amt_nett)
+                fo_journal_list.service =  to_decimal(output_list.service)
+                fo_journal_list.vat =  to_decimal(output_list.vat)
 
-            artikel = get_cache (Artikel, {"departement": [(eq, fo_journal_list.deptno)],"artnr": [(eq, fo_journal_list.artno)]})
+                artikel = get_cache (Artikel, {"departement": [(eq, fo_journal_list.deptno)],"artnr": [(eq, fo_journal_list.artno)]})
 
-            if artikel:
+                if artikel:
 
-                htparam = get_cache (Htparam, {"paramnr": [(eq, artikel.mwst_code)]})
+                    htparam = get_cache (Htparam, {"paramnr": [(eq, artikel.mwst_code)]})
 
-                if htparam:
-                    fo_journal_list.vat_percentage =  to_decimal(htparam.fdecimal)
-                else:
-                    fo_journal_list.vat_percentage =  to_decimal("0")
+                    if htparam:
+                        fo_journal_list.vat_percentage =  to_decimal(htparam.fdecimal)
+                    else:
+                        fo_journal_list.vat_percentage =  to_decimal("0")
 
-                htparam = get_cache (Htparam, {"paramnr": [(eq, artikel.service_code)]})
+                    htparam = get_cache (Htparam, {"paramnr": [(eq, artikel.service_code)]})
 
-                if htparam:
-                    fo_journal_list.serv_percentage =  to_decimal(htparam.fdecimal)
-                else:
-                    fo_journal_list.serv_percentage =  to_decimal("0")
+                    if htparam:
+                        fo_journal_list.serv_percentage =  to_decimal(htparam.fdecimal)
+                    else:
+                        fo_journal_list.serv_percentage =  to_decimal("0")
+
+    except Exception as e:
+        tb = traceback.format_exc()
+        lp.write_log("error",f"Exception occurred:\n{tb}\n","log_oscar.txt")
 
     return generate_output()
