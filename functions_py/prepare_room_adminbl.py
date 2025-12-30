2e3cd7c7eabfda75a9bf896bc293bbdbdda95f87
yusufwijasena_sindata <yusuf_wijasena@sindata.net>
2025-12-30 11:48:40 +0700

    fix: display room setup when zimkated is none

--- functions_py/prepare_room_adminbl.py
+++ functions_py/prepare_room_adminbl.py
@@ -1,4 +1,8 @@
-#using conversion tools version: 1.0.0.117
+# using conversion tools version: 1.0.0.117
+"""_yusufwijasena_30/12/2025
+
+        remark: - added validation to except when zimkateg is None
+"""
 
 from functions.additional_functions import *
 from decimal import Decimal
@@ -6,9 +10,10 @@ from datetime import date
 from functions.check_rm_limitbl import check_rm_limitbl
 from models import Zimmer, Htparam, Zimkateg
 
+
 def prepare_room_adminbl():
 
-    prepare_cache ([Htparam, Zimkateg])
+    prepare_cache([Htparam, Zimkateg])
 
     zikatnr = 0
     rmcatbez = ""
@@ -26,22 +31,28 @@ def prepare_room_adminbl():
 
     def generate_output():
         nonlocal zikatnr, rmcatbez, room_limit, curr_anz, ci_date, t_zimmer_data, zimmer, htparam, zimkateg
-
-
         nonlocal t_zimmer
         nonlocal t_zimmer_data
 
-        return {"zikatnr": zikatnr, "rmcatbez": rmcatbez, "room_limit": room_limit, "curr_anz": curr_anz, "ci_date": ci_date, "t-zimmer": t_zimmer_data}
-
+        return {
+            "zikatnr": zikatnr,
+            "rmcatbez": rmcatbez,
+            "room_limit": room_limit,
+            "curr_anz": curr_anz,
+            "ci_date": ci_date,
+            "t-zimmer": t_zimmer_data
+        }
 
     room_limit, curr_anz = get_output(check_rm_limitbl())
 
-    htparam = get_cache (Htparam, {"paramnr": [(eq, 87)]})
+    htparam = get_cache(Htparam, {"paramnr": [(eq, 87)]})
     ci_date = htparam.fdate
 
     zimkateg = db_session.query(Zimkateg).first()
-    zikatnr = zimkateg.zikatnr
-    rmcatbez = zimkateg.kurzbez
+
+    if zimkateg:
+        zikatnr = zimkateg.zikatnr
+        rmcatbez = zimkateg.kurzbez
 
     for zimmer in db_session.query(Zimmer).order_by(Zimmer.zinr).all():
         t_zimmer = T_zimmer()
