aa216eac6fe3d32083189dfca228162a307e1982
yusufwijasena_sindata <yusuf_wijasena@sindata.net>
2026-01-14 11:18:00 +0700

    fix: cannot create, modify, and delete item fix asset

--- functions_py/fa_artlist_m_list_name1bl.py
+++ functions_py/fa_artlist_m_list_name1bl.py
@@ -1,24 +1,24 @@
-#using conversion tools version: 1.0.0.117
+# using conversion tools version: 1.0.0.117
 
 from functions.additional_functions import *
 from decimal import Decimal
 from models import Mathis, Fa_artikel
 
-def fa_artlist_m_list_name1bl(m_list_name:string):
 
-    prepare_cache ([Mathis, Fa_artikel])
+def fa_artlist_m_list_name1bl(m_list_name: string):
+
+    prepare_cache([Mathis, Fa_artikel])
 
     mathis1_model = ""
     mathis1_supplier = ""
     do_it = False
-    fa_model:string = ""
-    fa_name:string = ""
+    fa_model: string = ""
+    fa_name: string = ""
     mathis = fa_artikel = None
 
     mathis1 = None
 
-    Mathis1 = create_buffer("Mathis1",Mathis)
-
+    Mathis1 = create_buffer("Mathis1", Mathis)
 
     db_session = local_storage.db_session
 
@@ -28,11 +28,13 @@ def fa_artlist_m_list_name1bl(m_list_name:string):
         nonlocal mathis1
         nonlocal mathis1
 
-        return {"mathis1_model": mathis1_model, "mathis1_supplier": mathis1_supplier, "do_it": do_it}
+        return {
+            "mathis1_model": mathis1_model,
+            "mathis1_supplier": mathis1_supplier,
+            "do_it": do_it
+        }
 
-
-    mathis1 = get_cache (Mathis, {"name": [(eq, m_list_name)]})
-    
+    mathis1 = get_cache(Mathis, {"name": [(eq, m_list_name)]})
 
     if mathis1:
         # print(f"[LOG] check asset: {mathis1}")
@@ -49,7 +51,7 @@ def fa_artlist_m_list_name1bl(m_list_name:string):
         mathis1_model = fa_model + fa_name
         mathis1_supplier = mathis1.supplier
 
-        fa_artikel = get_cache (Fa_artikel, {"nr": [(eq, mathis1.nr)]})
+        fa_artikel = get_cache(Fa_artikel, {"nr": [(eq, mathis1.nr)]})
 
         if fa_artikel:
 
@@ -57,6 +59,6 @@ def fa_artlist_m_list_name1bl(m_list_name:string):
                 do_it = True
 
     # else:
-        # print(f"[LOG] there is no Item") 
+        # print(f"[LOG] there is no Item")
 
     return generate_output()
