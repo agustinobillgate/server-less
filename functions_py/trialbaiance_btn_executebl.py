e12d3de15b0c70f5cfbc2006096155697286a2fa
Oscar Deladas <oscar_deladas@sindata.net>
2026-01-22 10:58:11 +0700

    Oscar - Fix trial balance slow

--- functions_py/trialbaiance_btn_executebl.py
+++ functions_py/trialbaiance_btn_executebl.py
@@ -8,6 +8,10 @@ from decimal import Decimal
 from datetime import date
 from functions.trialbalance_btn_go_cld_1bl import trialbalance_btn_go_cld_1bl
 
+from functions import log_program as lp
+
+import traceback
+
 def trialbaiance_btn_executebl(acct_type:int, from_fibu:string, to_fibu:string, sorttype:int, from_dept:int, from_date:date, to_date:date, close_month:int, close_date:date, pnl_acct:string, close_year:date, prev_month:int, show_longbal:bool, pbal_flag:bool, asremoteflag:bool):
     msg_str = ""
     tb_list_detail_data = []
@@ -38,142 +42,152 @@ def trialbaiance_btn_executebl(acct_type:int, from_fibu:string, to_fibu:string, 
 
         return {"msg_str": msg_str, "tb-list-detail": tb_list_detail_data, "tb-list-summary": tb_list_summary_data}
 
+    try:
+        output_list_data = get_output(trialbalance_btn_go_cld_1bl(acct_type, from_fibu, to_fibu, sorttype, from_dept, from_date, to_date, close_month, close_date, pnl_acct, close_year, prev_month, show_longbal, pbal_flag, asremoteflag))
 
-    output_list_data = get_output(trialbalance_btn_go_cld_1bl(acct_type, from_fibu, to_fibu, sorttype, from_dept, from_date, to_date, close_month, close_date, pnl_acct, close_year, prev_month, show_longbal, pbal_flag, asremoteflag))
+        if sorttype == 1:
 
-    if sorttype == 1:
+            for output_list in query(output_list_data):
 
-        for output_list in query(output_list_data):
-            refno = output_list.ref_no
-            begining_bal = output_list.begin_bal
-            tot_debit = output_list.tot_debit
-            tot_credit = output_list.tot_credit
-            net_change = output_list.net_change
-            ending_bal = output_list.ending_bal
+                refno = output_list.ref_no
+                begining_bal = output_list.begin_bal
+                tot_debit = output_list.tot_debit
+                tot_credit = output_list.tot_credit
+                net_change = output_list.net_change
+                ending_bal = output_list.ending_bal
+                note = output_list.note
 
-            if matches(begining_bal,r"*(*"):
-                begining_bal = replace_str(begining_bal, "(", "-")
+                if matches(begining_bal,r"*(*"):
+                    begining_bal = replace_str(begining_bal, "(", "-")
 
-            if matches(begining_bal,r"*)*"):
-                begining_bal = replace_str(begining_bal, ")", "")
+                if matches(begining_bal,r"*)*"):
+                    begining_bal = replace_str(begining_bal, ")", "")
 
-            if matches(tot_debit,r"*(*"):
-                tot_debit = replace_str(tot_debit, "(", "-")
+                if matches(tot_debit,r"*(*"):
+                    tot_debit = replace_str(tot_debit, "(", "-")
 
-            if matches(tot_debit,r"*)*"):
-                tot_debit = replace_str(tot_debit, ")", "")
+                if matches(tot_debit,r"*)*"):
+                    tot_debit = replace_str(tot_debit, ")", "")
 
-            if matches(tot_credit,r"*(*"):
-                tot_credit = replace_str(tot_credit, "(", "-")
+                if matches(tot_credit,r"*(*"):
+                    tot_credit = replace_str(tot_credit, "(", "-")
 
-            if matches(tot_credit,r"*)*"):
-                tot_credit = replace_str(tot_credit, ")", "")
+                if matches(tot_credit,r"*)*"):
+                    tot_credit = replace_str(tot_credit, ")", "")
 
-            if matches(net_change,r"*(*"):
-                net_change = replace_str(net_change, "(", "-")
+                if matches(net_change,r"*(*"):
+                    net_change = replace_str(net_change, "(", "-")
 
-            if matches(net_change,r"*)*"):
-                net_change = replace_str(net_change, ")", "")
+                if matches(net_change,r"*)*"):
+                    net_change = replace_str(net_change, ")", "")
 
-            if matches(ending_bal,r"*(*"):
-                ending_bal = replace_str(ending_bal, "(", "-")
+                if matches(ending_bal,r"*(*"):
+                    ending_bal = replace_str(ending_bal, "(", "-")
 
-            if matches(ending_bal,r"*)*"):
-                ending_bal = replace_str(ending_bal, ")", "")
+                if matches(ending_bal,r"*)*"):
+                    ending_bal = replace_str(ending_bal, ")", "")
 
-            tb_list_detail = Tb_list_detail()
-            tb_list_detail_data.append(tb_list_detail)
- 
-            tb_list_detail.marks = output_list.ch
+                tb_list_detail = Tb_list_detail()
+                tb_list_detail_data.append(tb_list_detail)
+    
+                tb_list_detail.marks = output_list.ch
 
-            if trim(substring(output_list.str, 0, 8)) != '':
-                dt = datetime.strptime(trim(substring(output_list.str, 0, 8)), "%m/%d/%y")
-                new_date_str = dt.strftime("%Y-%m-%d")
-            else:
-                new_date_str = trim(substring(output_list.str, 0, 8))
-                
-            tb_list_detail.date = new_date_str
-            tb_list_detail.ref_no = refno
-            tb_list_detail.begining_bal = begining_bal
-            tb_list_detail.tot_debit = tot_debit
-            tb_list_detail.tot_credit = tot_credit
-            tb_list_detail.net_change = net_change
-            tb_list_detail.ending_bal = ending_bal
-            tb_list_detail.note = substring(output_list.str, 134, 62)
+                if trim(substring(output_list.str, 0, 8)) != '':
+                    try:
+                        dt = datetime.strptime(trim(substring(output_list.str, 0, 8)), "%m/%d/%y")
+                        new_date_str = dt.strftime("%Y-%m-%d")
+                    except:
+                        new_date_str = ""
+                else:
+                    new_date_str = ""
+                    
+                tb_list_detail.date = new_date_str
+                tb_list_detail.ref_no = refno
+                tb_list_detail.begining_bal = begining_bal
+                tb_list_detail.tot_debit = tot_debit
+                tb_list_detail.tot_credit = tot_credit
+                tb_list_detail.net_change = net_change
+                tb_list_detail.ending_bal = ending_bal
+                # tb_list_detail.note = substring(output_list.str, 134, 62)
+                tb_list_detail.note = note
 
-            if output_list.is_show_depart :
-                tb_list_detail.dept_nr = to_string(output_list.dept_nr)
-                tb_list_detail.dept_name = output_list.dept_name
-            else:
-                tb_list_detail.dept_nr = ""
-                tb_list_detail.dept_name = ""
-    else:
+                if output_list.is_show_depart :
+                    tb_list_detail.dept_nr = to_string(output_list.dept_nr)
+                    tb_list_detail.dept_name = output_list.dept_name
+                else:
+                    tb_list_detail.dept_nr = ""
+                    tb_list_detail.dept_name = ""
+                    
+        else:
 
-        for output_list in query(output_list_data):
+            for output_list in query(output_list_data):
 
-            refno = substring(output_list.str, 0, 16)
-            begining_bal = (replace_str(substring(output_list.str, 54, 22) , ",", ""))
-            tot_debit = (replace_str(substring(output_list.str, 76, 22) , ",", ""))
-            tot_credit = (replace_str(substring(output_list.str, 98, 22) , ",", ""))
-            net_change = (replace_str(substring(output_list.str, 120, 22) , ",", ""))
-            ending_bal = (replace_str(substring(output_list.str, 142, 22) , ",", ""))
-            ytd_balance = (replace_str(substring(output_list.str, 164, 22) , ",", ""))
+                refno = substring(output_list.str, 0, 16)
+                begining_bal = (replace_str(substring(output_list.str, 54, 22) , ",", ""))
+                tot_debit = (replace_str(substring(output_list.str, 76, 22) , ",", ""))
+                tot_credit = (replace_str(substring(output_list.str, 98, 22) , ",", ""))
+                net_change = (replace_str(substring(output_list.str, 120, 22) , ",", ""))
+                ending_bal = (replace_str(substring(output_list.str, 142, 22) , ",", ""))
+                ytd_balance = (replace_str(substring(output_list.str, 164, 22) , ",", ""))
 
-            if matches(begining_bal,r"*(*"):
-                begining_bal = replace_str(begining_bal, "(", "-")
+                if matches(begining_bal,r"*(*"):
+                    begining_bal = replace_str(begining_bal, "(", "-")
 
-            if matches(begining_bal,r"*)*"):
-                begining_bal = replace_str(begining_bal, ")", "")
+                if matches(begining_bal,r"*)*"):
+                    begining_bal = replace_str(begining_bal, ")", "")
 
-            if matches(tot_debit,r"*(*"):
-                tot_debit = replace_str(tot_debit, "(", "-")
+                if matches(tot_debit,r"*(*"):
+                    tot_debit = replace_str(tot_debit, "(", "-")
 
-            if matches(tot_debit,r"*)*"):
-                tot_debit = replace_str(tot_debit, ")", "")
+                if matches(tot_debit,r"*)*"):
+                    tot_debit = replace_str(tot_debit, ")", "")
 
-            if matches(tot_credit,r"*(*"):
-                tot_credit = replace_str(tot_credit, "(", "-")
+                if matches(tot_credit,r"*(*"):
+                    tot_credit = replace_str(tot_credit, "(", "-")
 
-            if matches(tot_credit,r"*)*"):
-                tot_credit = replace_str(tot_credit, ")", "")
+                if matches(tot_credit,r"*)*"):
+                    tot_credit = replace_str(tot_credit, ")", "")
 
-            if matches(net_change,r"*(*"):
-                net_change = replace_str(net_change, "(", "-")
+                if matches(net_change,r"*(*"):
+                    net_change = replace_str(net_change, "(", "-")
 
-            if matches(net_change,r"*)*"):
-                net_change = replace_str(net_change, ")", "")
+                if matches(net_change,r"*)*"):
+                    net_change = replace_str(net_change, ")", "")
 
-            if matches(ending_bal,r"*(*"):
-                ending_bal = replace_str(ending_bal, "(", "-")
+                if matches(ending_bal,r"*(*"):
+                    ending_bal = replace_str(ending_bal, "(", "-")
 
-            if matches(ending_bal,r"*)*"):
-                ending_bal = replace_str(ending_bal, ")", "")
+                if matches(ending_bal,r"*)*"):
+                    ending_bal = replace_str(ending_bal, ")", "")
 
-            if matches(ytd_balance,r"*(*"):
-                ytd_balance = replace_str(ytd_balance, "(", "-")
+                if matches(ytd_balance,r"*(*"):
+                    ytd_balance = replace_str(ytd_balance, "(", "-")
 
-            if matches(ytd_balance,r"*)*"):
-                ytd_balance = replace_str(ytd_balance, ")", "")
+                if matches(ytd_balance,r"*)*"):
+                    ytd_balance = replace_str(ytd_balance, ")", "")
+                    
+                tb_list_summary = Tb_list_summary()
+                tb_list_summary_data.append(tb_list_summary)
 
-            tb_list_summary = Tb_list_summary()
-            tb_list_summary_data.append(tb_list_summary)
+                tb_list_summary.account_no = trim(refno)
+                tb_list_summary.description = trim(substring(output_list.str, 16, 38))
+                tb_list_summary.beginingbal = begining_bal
+                tb_list_summary.tot_debit = tot_debit
+                tb_list_summary.tot_credit = tot_credit
+                tb_list_summary.net_change = net_change
+                tb_list_summary.ending_bal = ending_bal
+                tb_list_summary.ytd_balance = ytd_balance
+                tb_list_summary.budget =  to_decimal(output_list.budget)
+                tb_list_summary.proz =  to_decimal(output_list.proz)
 
-            tb_list_summary.account_no = trim(refno)
-            tb_list_summary.description = trim(substring(output_list.str, 16, 38))
-            tb_list_summary.beginingbal = begining_bal
-            tb_list_summary.tot_debit = tot_debit
-            tb_list_summary.tot_credit = tot_credit
-            tb_list_summary.net_change = net_change
-            tb_list_summary.ending_bal = ending_bal
-            tb_list_summary.ytd_balance = ytd_balance
-            tb_list_summary.budget =  to_decimal(output_list.budget)
-            tb_list_summary.proz =  to_decimal(output_list.proz)
+                if output_list.is_show_depart :
+                    tb_list_summary.dept_nr = to_string(output_list.dept_nr)
+                    tb_list_summary.dept_name = output_list.dept_name
+                else:
+                    tb_list_summary.dept_nr = ""
+                    tb_list_summary.dept_name = ""
 
-            if output_list.is_show_depart :
-                tb_list_summary.dept_nr = to_string(output_list.dept_nr)
-                tb_list_summary.dept_name = output_list.dept_name
-            else:
-                tb_list_summary.dept_nr = ""
-                tb_list_summary.dept_name = ""
+    except Exception as e:
+        lp.write_log("error", f"error: {traceback.format_exc()}")
 
     return generate_output()
